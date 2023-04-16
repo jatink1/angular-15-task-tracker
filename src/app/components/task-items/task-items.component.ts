2609ee32083750ffb6b75b9b9854d2c3204f2951
Jatin Kumawat <jatink@DESKTOP-H9MFB1F>
2023-04-17 00:45:00 +0530

    Intial Commit

---
+++ src/app/components/task-items/task-items.component.ts
@@ -0,0 +1,29 @@
+import {
+  Component,
+  EnvironmentInjector,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-items',
+  templateUrl: './task-items.component.html',
+  styleUrls: ['./task-items.component.css'],
+})
+export class TaskItemsComponent {
+  @Input() task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
+  }
+}
